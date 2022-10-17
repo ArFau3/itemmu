3916e591d82b01c19598ffe0c55f2113a505c344
ArFau3 <h1101201020@student.untan.ac.id>
2022-10-17 10:38:37 +0700

    inisialisasi project

---
+++ lib/ui/awal.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AwalScreen extends StatelessWidget {
+  const AwalScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('gi'),
+    ));
+  }
+
+}
